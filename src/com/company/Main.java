820d87f4fdfef5c3caf40c41f1762df6f7b9effa
Rafael Binnenmarsch <rjbinnen@avans.nl>
2020-01-14 14:30:59 +0100

    Basis GUI op gezet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
+        launch(args);
 	// write your code here
-        System.out.println("Raffy heeft homo");
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane borderPane = new BorderPane();
+        Scene homeScene = new Scene(borderPane);
+        primaryStage.setTitle("Netflix Statistics - Home");
+        primaryStage.setScene(homeScene);
+        primaryStage.show();
     }
 }
