312defd8dcf590d5c2695842024456a581f4634a
RubenKraaijeveld <50320054+RubenKraaijeveld@users.noreply.github.com>
2020-01-15 14:03:52 +0100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ package com.company;
         public static void main(String[] args) {
 
             // write your code here
-
+            System.out.println("Als je dit leest lars dan werkt git");
         }
     }
 
