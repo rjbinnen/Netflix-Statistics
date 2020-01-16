75d907ee865a39bc44e0571bea020ba16e3f78aa
RubenKraaijeveld <50320054+RubenKraaijeveld@users.noreply.github.com>
2020-01-16 13:07:08 +0100

    test

--- src/com/company/GUI/Controllers/LoginController.java
+++ src/com/company/GUI/Controllers/LoginController.java
@@ -1,17 +1,20 @@
-package com.company.GUI;
+package com.company.GUI.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController implements EventHandler<ActionEvent> {
     private TextField field;
     private PasswordField pwfield;
+    private Label label;
 
-    public LoginController(TextField field, PasswordField pwfield) {
+    public LoginController(TextField field, PasswordField pwfield, Label label) {
     this.field = field;
     this.pwfield = pwfield;
+    this.label = label;
     }
 
 
@@ -20,11 +23,11 @@ public class LoginController implements EventHandler<ActionEvent> {
         String email = "Blah@gmail.com";
         String pw = "Beepboop";
         if (field.getText().equals(email) && pwfield.getText().equals(pw)){
-            System.out.println("Beep Boop");
+            label.setText("Beep Boop");
         } else {
             field.clear();
             pwfield.clear();
-            System.out.println("ERROR 404: Intelligence Not Found!");
+            label.setText("ERROR 404: Intelligence Not Found!");
         }
 
     }
