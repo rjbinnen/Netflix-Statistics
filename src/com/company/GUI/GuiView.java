f1920d7006ef35a7cfa83ca58ed6e792196a0bf3
ljawj <59871883+ljawj@users.noreply.github.com>
2020-01-16 11:14:47 +0100

    test

--- src/com/company/GUI/GuiView.java
+++ src/com/company/GUI/GuiView.java
@@ -1,11 +1,14 @@
 package com.company.GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GuiView extends Application {
@@ -16,16 +19,31 @@ public class GuiView extends Application {
         Scene homeScene = new Scene(gridPaneHome);
         primaryStage.setTitle("Netflix Statistics - Home");
 
+        Text textEmail = new Text("E-mail");
+        Text textPassword = new Text("Password");
+
+        TextField textFieldEmail = new TextField();
+        PasswordField textFieldPassword = new PasswordField();
+
+        Button logInButton = new Button("Log in");
         Button registerButton = new Button("Register");
-        gridPaneHome.getChildren().addAll(registerButton);
-        gridPaneHome.setConstraints(registerButton, 10, 2);
+
+        gridPaneHome.setMinSize(400, 200);
+        gridPaneHome.setPadding(new Insets(10, 10, 10, 10));
+        gridPaneHome.setVgap(5);
+        gridPaneHome.setHgap(5);
+        gridPaneHome.setAlignment(Pos.CENTER);
+
+        gridPaneHome.add(textEmail, 0, 0);
+        gridPaneHome.add(textFieldEmail, 1, 0);
+        gridPaneHome.add(textPassword, 0, 1);
+        gridPaneHome.add(textFieldPassword, 1, 1);
+        gridPaneHome.add(logInButton, 0, 2);
+        gridPaneHome.add(registerButton, 1, 2);
+
 
         primaryStage.setScene(homeScene);
         primaryStage.show();
-
-        BorderPane borderPaneRegister = new BorderPane();
-        Scene register = new Scene(borderPaneRegister);
-
 
     }
 }
