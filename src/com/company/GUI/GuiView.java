0f943ee863451715ddfc5c5d8b9cda6cd739c74c
ljawj <59871883+ljawj@users.noreply.github.com>
2020-01-16 10:54:30 +0100

    test

--- src/com/company/GUI/GuiView.java
+++ src/com/company/GUI/GuiView.java
@@ -2,18 +2,31 @@ package com.company.GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GuiView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane borderPane = new BorderPane();
-        Scene homeScene = new Scene(borderPane);
+        GridPane gridPaneHome = new GridPane();
+        Scene homeScene = new Scene(gridPaneHome);
         primaryStage.setTitle("Netflix Statistics - Home");
+
+        Button registerButton = new Button("Register");
+        gridPaneHome.getChildren().addAll(registerButton);
+        gridPaneHome.setConstraints(registerButton, 10, 2);
+
         primaryStage.setScene(homeScene);
         primaryStage.show();
+
+        BorderPane borderPaneRegister = new BorderPane();
+        Scene register = new Scene(borderPaneRegister);
+
+
     }
 }
 
