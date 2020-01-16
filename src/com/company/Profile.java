bf9e23634345db2cbf1ec8a0916e90e0926e1f34
RubenKraaijeveld <50320054+RubenKraaijeveld@users.noreply.github.com>
2020-01-16 10:11:55 +0100

    test

--- src/com/company/Profile.java
+++ src/com/company/Profile.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Profile {
-    private String naam;
-    private Date geboortedatum;
+    private String name;
+    private Date dateOfBirth;
     private ArrayList<Program> watchedPrograms;
+
+    public Profile (String name, Date dateOfBirth, ArrayList<Program> watchedPrograms) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.watchedPrograms = watchedPrograms;
+    }
 
 }
