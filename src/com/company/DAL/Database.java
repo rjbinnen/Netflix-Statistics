347125bb2810472c44d641bb411f169fffab4fa7
Rafael Binnenmarsch <rjbinnen@avans.nl>
2020-04-08 17:00:03 +0200

    Database klasse werkend gekregen

--- src/com/company/DAL/Database.java
+++ src/com/company/DAL/Database.java
@@ -1,6 +1,64 @@
 package com.company.DAL;
 
-public class Database {
-    private String connectionString = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=NetflixStatistix;integratedSecurity=true;";
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Database {
+    private String connectionUrl = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=NetflixStatistix;integratedSecurity=true;";
+
+    public Database() {
+        // Connection beheert informatie over de connectie met de database.
+        Connection con = null;
+
+        // Statement zorgt dat we een SQL query kunnen uitvoeren.
+        Statement stmt = null;
+
+        // ResultSet is de tabel die we van de database terugkrijgen.
+        // We kunnen door de rows heen stappen en iedere kolom lezen.
+        ResultSet rs = null;
+
+        try {
+            // 'Importeer' de driver die je gedownload hebt.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Maak de verbinding met de database.
+            con = DriverManager.getConnection(connectionUrl);
+
+            // Stel een SQL query samen.
+            String SQL = "SELECT TOP 10 * FROM Movie";
+            stmt = con.createStatement();
+            // Voer de query uit op de database.
+            rs = stmt.executeQuery(SQL);
+
+            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+
+            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
+            while (rs.next()) {
+                // Vraag per row de kolommen in die row op.
+                int ISBN = rs.getInt("ISBN");
+                String title = rs.getString("Titel");
+                String author = rs.getString("Auteur");
+
+                // Print de kolomwaarden.
+                // System.out.println(ISBN + " " + title + " " + author);
+
+                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
+                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
+                System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
+            }
+            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+            if (con != null) try { con.close(); } catch(Exception e) {}
+        }
+    }
 }
--- src/com/company/GUI/Views/View.java
+++ src/com/company/GUI/Views/View.java
@@ -1,5 +1,6 @@
 package com.company.GUI.Views;
 
+import com.company.DAL.Database;
 import com.company.Domain.Account;
 import com.company.Domain.Episode;
 import com.company.Domain.Profile;
@@ -27,62 +28,9 @@ public class View extends Application {
     @SuppressWarnings("Duplicates")
     public void start(Stage primaryStage) throws Exception {
 
-
         //testcode db connectie
-        // Dit zijn de instellingen voor de verbinding. Vervang de databaseName indien deze voor jou anders is.
-        String connectionUrl = "jdbc:sqlserver://localhost\\MSSQLSERVER;databaseName=NetflixStatistix;integratedSecurity=true;";
+        Database database = new Database();
 
-        // Connection beheert informatie over de connectie met de database.
-        Connection con = null;
-
-        // Statement zorgt dat we een SQL query kunnen uitvoeren.
-        Statement stmt = null;
-
-        // ResultSet is de tabel die we van de database terugkrijgen.
-        // We kunnen door de rows heen stappen en iedere kolom lezen.
-        ResultSet rs = null;
-
-        try {
-            // 'Importeer' de driver die je gedownload hebt.
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            // Maak de verbinding met de database.
-            con = DriverManager.getConnection(connectionUrl);
-
-            // Stel een SQL query samen.
-            String SQL = "SELECT TOP 10 * FROM Movie";
-            stmt = con.createStatement();
-            // Voer de query uit op de database.
-            rs = stmt.executeQuery(SQL);
-
-            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-
-            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
-            while (rs.next()) {
-                // Vraag per row de kolommen in die row op.
-                int ISBN = rs.getInt("ISBN");
-                String title = rs.getString("Titel");
-                String author = rs.getString("Auteur");
-
-                // Print de kolomwaarden.
-                // System.out.println(ISBN + " " + title + " " + author);
-
-                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
-                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
-                System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
-            }
-            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
-
-        }
-
-        // Handle any errors that may have occurred.
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            if (rs != null) try { rs.close(); } catch(Exception e) {}
-            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
-            if (con != null) try { con.close(); } catch(Exception e) {}
-        }
 
         //testcode percWatchTime van episode
         Episode episode = new Episode(5, 60, 30);
